f2cb1ad97a5510b34a1964909e6c5b5a99283f6e
belikova-julia <alistami38@gmail.com>
2021-12-25 01:29:45 +0300

    Scripted auto-commit on change (2021-12-25 01:29:45) by gitwatch.sh

--- src/main/java/lab6/HttpServer.java
+++ src/main/java/lab6/HttpServer.java
@@ -1,10 +1,22 @@
 package lab6;
 
+import akka.actor.ActorRef;
+import akka.http.javadsl.Http;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
 public class HttpServer implements Watcher {
-    public HttpServer()
+    private final Http http;
+    private final ActorRef configurator;
+    private final ZooKeeper zoo;
+
+
+    public HttpServer(Http http, ActorRef configurator, ZooKeeper zoo, String port) {
+        this.http = http;
+        this.configurator = configurator;
+        this.zoo = zoo;
+    }
     @Override
     public void process(WatchedEvent watchedEvent) {
 
