5535350cf1ad6e4d966c2d2030d6a0ff12941ec9
belikova-julia <alistami38@gmail.com>
2021-12-25 01:24:33 +0300

    Scripted auto-commit on change (2021-12-25 01:24:33) by gitwatch.sh

--- src/main/java/lab6/HttpServer.java
+++ src/main/java/lab6/HttpServer.java
@@ -4,6 +4,7 @@ import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class HttpServer implements Watcher {
+    public HttpServer()
     @Override
     public void process(WatchedEvent watchedEvent) {
 
