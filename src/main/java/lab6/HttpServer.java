93304a46c1912b6d8961729c42a18979ee9906da
belikova-julia <alistami38@gmail.com>
2021-12-25 01:35:13 +0300

    Scripted auto-commit on change (2021-12-25 01:35:13) by gitwatch.sh

--- src/main/java/lab6/HttpServer.java
+++ src/main/java/lab6/HttpServer.java
@@ -2,21 +2,29 @@ package lab6;
 
 import akka.actor.ActorRef;
 import akka.http.javadsl.Http;
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+
+import java.nio.charset.StandardCharsets;
 
 public class HttpServer implements Watcher {
     private final Http http;
     private final ActorRef configurator;
     private final ZooKeeper zoo;
 
+    private static String SERVERS_PATH = "localhost:";
 
-    public HttpServer(Http http, ActorRef configurator, ZooKeeper zoo, String port) {
+
+    public HttpServer(Http http, ActorRef configurator, ZooKeeper zoo, String port) throws InterruptedException, KeeperException {
         this.http = http;
         this.configurator = configurator;
         this.zoo = zoo;
+        String serverPath = SERVERS_PATH + port;
+        zoo.create("/servers/" + serverPath,
+                serverPath.getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL);
     }
+    
     @Override
     public void process(WatchedEvent watchedEvent) {
 
