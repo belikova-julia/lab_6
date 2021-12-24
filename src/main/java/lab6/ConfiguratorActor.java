81914c8aa18a6489614ce7138a90b3dbe355767b
belikova-julia <alistami38@gmail.com>
2021-12-25 01:06:29 +0300

    Scripted auto-commit on change (2021-12-25 01:06:29) by gitwatch.sh

--- src/main/java/lab6/ConfiguratorActor.java
+++ src/main/java/lab6/ConfiguratorActor.java
@@ -2,9 +2,16 @@ package lab6;
 
 import akka.actor.AbstractActor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConfiguratorActor extends AbstractActor {
+    private List<String> servers = new ArrayList<>();
     @Override
     public Receive createReceive() {
-        return null;
+        return receiveBuilder()
+                .match()
+                .match()
+                .build();
     }
 }
