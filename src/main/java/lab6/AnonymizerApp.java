7ec9c095dc7979859194ef5f8d3faa7817ec5582
belikova-julia <alistami38@gmail.com>
2021-12-25 03:46:59 +0300

    Scripted auto-commit on change (2021-12-25 03:46:59) by gitwatch.sh

--- src/main/java/lab6/AnonymizerApp.java
+++ src/main/java/lab6/AnonymizerApp.java
@@ -1,12 +1,22 @@
 package lab6;
 
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.Http;
+import akka.stream.ActorMaterializer;
 import org.apache.log4j.BasicConfigurator;
+import org.apache.zookeeper.ZooKeeper;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 public class AnonymizerApp {
-    private static final String SYSTEM_NAME = "anonymizer"
+    private static final String SYSTEM_NAME = "anonymizer";
+    private static final int TIMEOUT = 5000;
+
+
+
     public static void main(String[] args) {
         if (args.length < 2) {
             System.err.println("Incorrect arguments amount");
@@ -16,6 +26,14 @@ public class AnonymizerApp {
         BasicConfigurator.configure();
         System.out.println("Start with ports: " + Arrays.toString(args));
         ActorSystem system = ActorSystem.create(SYSTEM_NAME);
-        
+        ActorRef configurator = system.actorOf(Props.create(ConfiguratorActor.class));
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        ZooKeeper zooKeeper = null;
+
+        zooKeeper = new ZooKeeper(args[0], TIMEOUT,null);
+        new ZooKeeperWatcher(zooKeeper, configurator);
+
     }
 }
