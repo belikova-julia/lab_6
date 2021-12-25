d49532d6dd67ec92f079f0c46b1d5db284ada0b3
belikova-julia <alistami38@gmail.com>
2021-12-25 03:35:44 +0300

    Scripted auto-commit on change (2021-12-25 03:35:44) by gitwatch.sh

--- src/main/java/lab6/AnonymizerApp.java
+++ src/main/java/lab6/AnonymizerApp.java
@@ -1,4 +1,21 @@
 package lab6;
 
+import akka.actor.ActorSystem;
+import org.apache.log4j.BasicConfigurator;
+
+import java.util.Arrays;
+
 public class AnonymizerApp {
+    private static final String SYSTEM_NAME = "anonymizer"
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Incorrect arguments amount");
+            System.exit(-1);
+        }
+
+        BasicConfigurator.configure();
+        System.out.println("Start with ports: " + Arrays.toString(args));
+        ActorSystem system = ActorSystem.create(SYSTEM_NAME);
+        
+    }
 }
